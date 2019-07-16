bca4f8441467d702f068f6c44af6cd8c09daa4e9
Somesh Kar <someshkar04@gmail.com>
2019-07-16 22:32:50 +0530

    Fix source code problem

--- basic_crawler/without-bs4.py
+++ basic_crawler/without-bs4.py
@@ -21,16 +21,18 @@ class Crawler:
             if link.startswith('mailto:') or link.startswith('tel:'):
                 links.remove(link)
 
-        return title[0], links, current_time
+        return title[0], links, current_time, data
 
 
 # URL to crawl
 URL = 'https://stallman.org'
 
 c = Crawler(URL)
-title, links, current_time = c.crawl()
+title, links, current_time, source_code = c.crawl()
 
 print('Title: {}'.format(title))
 
 for link in links:
     print('Found URL: {} at {}'.format(link, current_time))
+
+print('\nComplete source code: {}'.format(source_code))
