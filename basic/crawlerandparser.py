9c3abaa4c8eaa0e3cfcb548621150a25e5bbfe70
Somesh Kar <someshkar04@gmail.com>
2019-07-17 17:32:02 +0530

    Finish fully featured crawler

--- basic/crawlerandparser.py
+++ basic/crawlerandparser.py
@@ -1,5 +1,7 @@
 import re
 import requests
+import random
+import time
 from datetime import datetime
 
 
@@ -18,6 +20,20 @@ class Crawler:
         title = re.findall(r'<title>(.*?)</title>', str(main_source))[0]
         links = re.findall(r'<a href="(.*?)"', str(main_source))
 
+        robots = requests.get(self.url + '/robots.txt').text
+
+        if robots is not None:
+            for instruction in robots.splitlines():
+                print(instruction)
+                if instruction.startswith('Allow:'):
+                    subfolder = re.findall(
+                        r'Allow: (.*?)', str(instruction))[0]
+                    links.append(self.url + subfolder)
+                if instruction.startswith('Disallow:'):
+                    for link in links:
+                        if instruction in link:
+                            links.remove(link)
+
         for link in links:
             if link.startswith('mailto:') or link.startswith('tel:') \
                or link.startswith('#'):
@@ -35,6 +51,8 @@ class Crawler:
                 link_data['link'], link_data['title']))
 
             links_and_data.append(link_data)
+
+            time.sleep(random.randint(1, 5))
 
         return title, links_and_data, current_time, main_source
 
